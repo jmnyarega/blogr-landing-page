2e19f87d5e5fb380410e3af1ff12a9ee319b3a66
josiahmokob0 <jmnyarega@gmail.com>
2021-06-02 13:12:22 +0300

    ham + css refactor with scss

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const ham = document.getElementById("hamburger");
+
+const selectHam = () => {
+  const classes = Array.from(ham.classList);
+  const closed = classes.includes("hamburger-closed");
+  const open = classes.includes("hamburger-open");
+  const close_img = document.querySelector(".hamburger img:last-child");
+  const open_img = document.querySelector(".hamburger img:first-child");
+
+  if (closed) {
+    ham.classList.remove("hamburger-closed");
+    ham.classList.add("hamburger-open");
+    open_img.style.display = "block";
+    close_img.style.display = "none";
+  }
+
+  if (open) {
+    ham.classList.remove("hamburger-open");
+    ham.classList.add("hamburger-closed");
+    open_img.style.display = "none";
+    close_img.style.display = "block";
+  }
+};
